069547f34f6afd44bf8c26680c8e206f3900595f
Imran Shitta-Bey <pickleboyonline@gmail.com>
2023-06-23 02:03:49 -0500

    cleanup

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -3,4 +3,28 @@
  * Typescript/TSX compiler friendly it's aliased as a
  * `JSX.Element`
  */
-export type SolidNativeElement = JSX.Element
+export type SolidNativeElement = JSX.Element;
+
+// Define types internally
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [name: string]: Record<string, unknown>;
+    }
+    interface Element {
+      firstChild?: Element;
+      parentElement?: Element;
+      setProp(name: string, value: unknown): void;
+      isTextElement: boolean;
+      removeChild(element: Element): void;
+      insertBefore(element: Element, anchor?: Element): void;
+      next?: Element;
+      prev?: Element;
+    }
+
+    interface ElementChildrenAttribute {
+      children?: {}; // specify children name to use
+    }
+  }
+}
