71e0cbd0d03754d2e141124a25ba3300d8e377ac
Imran Shitta-Bey <pickleboyonline@gmail.com>
2023-06-23 02:20:02 -0500

    del

--- references/example_expo_module/android/src/main/java/expo/modules/testexpomodule/TestExpoModuleView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.testexpomodule
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class TestExpoModuleView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
