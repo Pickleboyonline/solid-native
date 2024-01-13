528db1d4f7264af0d3248fb4b2c94b3ff95258d0
Imran Shitta-Bey <ias45@cornell.edu>
2024-01-12 19:46:40 -0600

    add

---
+++ packages/new_ios/SolidNative/SolidNative/SolidNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidNativeApp.swift
+//  SolidNative
+//
+//  Created by Imran Shitta-Bey on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SolidNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
