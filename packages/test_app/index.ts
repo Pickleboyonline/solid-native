80fe4b2c98c3fdf63ee8b6b217e1f1839cb079a4
Imran Shitta-Bey <ias45@cornell.edu>
2024-01-12 19:37:06 -0600

    fixes

--- packages/test_app/index.ts
+++ packages/test_app/index.ts
@@ -1,4 +1,5 @@
 import { SolidNativeCore, render } from "../core/mod.ts";
-import { App } from "./ios/App.tsx";
-
+import { App } from "./App.tsx";
+// deno-lint-ignore ban-ts-comment
+// @ts-ignore
 render(App, SolidNativeCore.getRootElement());
