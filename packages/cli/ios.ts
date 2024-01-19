80da68bafb82017270c09ee71bdd09e6ce6c3df4
Imran Shitta-Bey <ias45@cornell.edu>
2024-01-18 20:20:54 -0600

    stuff

--- packages/cli/ios.ts
+++ packages/cli/ios.ts
@@ -1,7 +1,7 @@
 import xcode from "npm:xcode@3.0.1";
 // Source: https://github.dev/apache/cordova-node-xcode
 const path = import.meta
-  .resolve("./../new_ios/SolidNative/SolidNative.xcodeproj/project.pbxproj")
+  .resolve("./../new_ios/SolidNative.xcodeproj/project.pbxproj")
   .replace("file://", "");
 // Another xcode manager.
 // import { Xcode } from "npm:pbxproj-dom@1.2.0/xcode.js";
--- packages/new_ios/SolidNative/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  SolidNative
-//
-//  Created by Imran Shitta-Bey on 1/12/24.
-//
-
-import Foundation
